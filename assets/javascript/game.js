34b681d8a2c1da30b528c3c203bceea548c3526a
edean78 <edean78@gmail.com>
2019-09-16 00:06:11 -0400

    Created a function to calculate the random target score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,37 @@
 // Ready the document
-$(document).ready(function () {
-    console.log("Ready");
-});
+// $(document).ready(function () {
+//     console.log("Ready");
+// });
 
 // Create Variables
 // ===========================================================================================
 
-var totalScore: 0;
-var targerScore: 0;
+var totalScore = 0;
+var targetScore = randomNumber;
 var wins = 0;
 var losses = 0;
+var minNumber = 19;
+var maxNumber = 120;
+var randomNumber = targetScoreRandom(minNumber, maxNumber);
 
-var crystal = {
-    color:
-    randomNum:
-}
+// var crystal = {
+//     color:
+//     randomNum: 0;
+// }
 
 // Create Function and Logic
 // ============================================================================================
 
-function initGame() {
-    var minNumber = 19;
-    var maxNumber = 120;
-
-    targetScore = Math.floor(Math.random)
+// Create a function to calculate the Target Score
+function targetScoreRandom(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+// Console log the random number
+console.log(randomNumber);
+
+$("#tarScore").html(targetScore);
+
+
+
