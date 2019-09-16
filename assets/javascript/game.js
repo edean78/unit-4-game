5b51cc046d07d4a8f3a11f3407544cec64dbecd3
edean78 <edean78@gmail.com>
2019-09-15 23:34:41 -0400

    Created the overall look of the application

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// Ready the document
+$(document).ready(function () {
+    console.log("Ready");
+});
+
+// Create Variables
+// ===========================================================================================
+
+var totalScore: 0;
+var targerScore: 0;
+var wins = 0;
+var losses = 0;
+
+var crystal = {
+    color:
+    randomNum:
+}
+
+// Create Function and Logic
+// ============================================================================================
+
+function initGame() {
+    var minNumber = 19;
+    var maxNumber = 120;
+
+    targetScore = Math.floor(Math.random)
+}
