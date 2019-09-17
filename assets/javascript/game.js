9fa1bbfc8771943f6ebde03bbfdec693496a3456
edean78 <edean78@gmail.com>
2019-09-17 14:44:45 -0400

    Created objects for each crytal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,43 @@
 // Ready the document
-// $(document).ready(function () {
-//     console.log("Ready");
+// $(document).ready(function() {
+
+
+    // Create Variables
+    // ===========================================================================================
+
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var minNumber = 19;
+    var maxNumber = 120;
+    var targetScore = targetScoreRandom(minNumber, maxNumber);
+
+    // Create Function and Logic
+    // ============================================================================================
+
+    // Create function to create objects of each crystal
+    function Crystal(color, picURL, ranVal) {
+        this.color = color;
+        this.picURL = picURL;
+        this.ranVal = ranVal;
+    }
+
+    // Create each instance of each crystal
+    var crystalYellow = new Crystal("yellow", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png", 0);
+    var crystalBlue = new Crystal("blue", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png", 0);
+    var crystalGreen = new Crystal("green", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png", 0);
+    var crystalPurple = new Crystal("purple", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png", 0);
+
+    
+
+    // Create a function to calculate the Target Score
+    function targetScoreRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    console.log(targetScore);
+
+    $("#tarScore").text(targetScore);
+    var objectRandom = Math.floor((Math.random() * 12) + 1);
+
+    $.each()
 // });
-
-// Create Variables
-// ===========================================================================================
-
-var totalScore = 0;
-var targetScore = randomNumber;
-var wins = 0;
-var losses = 0;
-var minNumber = 19;
-var maxNumber = 120;
-var randomNumber = targetScoreRandom(minNumber, maxNumber);
-
-// var crystal = {
-//     color:
-//     randomNum: 0;
-// }
-
-// Create Function and Logic
-// ============================================================================================
-
-// Create a function to calculate the Target Score
-function targetScoreRandom(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-// Console log the random number
-console.log(randomNumber);
-
-$("#tarScore").html(targetScore);
-
-
-
