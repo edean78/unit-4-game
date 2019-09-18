a726c60d2d5f4e7f247b56c79481bc3354b3d375
edean78 <edean78@gmail.com>
2019-09-18 11:20:31 -0400

    Added the object back to the js file. Having an issue when the game reset, new values are not being added to the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,25 +14,25 @@ $(document).ready(function () {
     // Create Function and Logic
     // ============================================================================================
 
-    // // Create function to create objects of each crystal
-    // function Crystal(color, picURL, ranVal) {
-    //     this.color = color;
-    //     this.picURL = picURL;
-    //     this.ranVal = ranVal;
-    // }
+    // Create function to create objects of each crystal
+    function Crystal(color, picURL, ranVal) {
+        this.color = color;
+        this.picURL = picURL;
+        this.ranVal = ranVal;
+    }
 
-    // // Create an instance of each crystal
-    // var crystalYellow = new Crystal("yellow", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png", "0");
-    // var crystalBlue = new Crystal("blue", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/blue_crystal-512.png", "0");
-    // var crystalGreen = new Crystal("green", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/green_crystal_copy-512.png", "0");
-    // var crystalPurple = new Crystal("purple", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2m528Ek7hzOAhQVNGbzQPQHAWdX160irwI3Q9hctg7XmaZ1d", "0");
+    // Create an instance of each crystal
+    var crystalYellow = new Crystal("yellow", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png", "0");
+    var crystalBlue = new Crystal("blue", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/blue_crystal-512.png", "0");
+    var crystalGreen = new Crystal("green", "https://cdn1.iconfinder.com/data/icons/crystal-1/60/green_crystal_copy-512.png", "0");
+    var crystalPurple = new Crystal("purple", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2m528Ek7hzOAhQVNGbzQPQHAWdX160irwI3Q9hctg7XmaZ1d", "0");
 
-    // // Create an array of the objects
-    // var crystalArray = [
-    //     crystalYellow,
-    //     crystalBlue,
-    //     crystalGreen,
-    //     crystalPurple
+    // Create an array of the objects
+    var crystalArray = [
+        crystalYellow,
+        crystalBlue,
+        crystalGreen,
+        crystalPurple
     ]
 
     function initGame() {
@@ -40,13 +40,13 @@ $(document).ready(function () {
         // Empty the crystals
         $("#crystals").empty();
 
-        // Create an array to hold the image url
-        var img = [
-            "https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png",
-            "https://cdn1.iconfinder.com/data/icons/crystal-1/60/blue_crystal-512.png",
-            "https://cdn1.iconfinder.com/data/icons/crystal-1/60/green_crystal_copy-512.png",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2m528Ek7hzOAhQVNGbzQPQHAWdX160irwI3Q9hctg7XmaZ1d"
-        ]
+        // // Create an array to hold the image url
+        // var img = [
+        //     'https://cdn1.iconfinder.com/data/icons/crystal-1/60/yellow_crystal-512.png',
+        //     'https://cdn1.iconfinder.com/data/icons/crystal-1/60/blue_crystal-512.png',
+        //     'https://cdn1.iconfinder.com/data/icons/crystal-1/60/green_crystal_copy-512.png',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2m528Ek7hzOAhQVNGbzQPQHAWdX160irwI3Q9hctg7XmaZ1d'
+        // ]
 
         // Set targetScore variable to a random number between 19 - 120
         targetScore = Math.floor(Math.random() * (120 - 19 + 1) + 19);
@@ -70,7 +70,7 @@ $(document).ready(function () {
             imgCrystal.addClass("crystal-image");
 
             // Added the src and url to the image element
-            imgCrystal.attr("src", img[i]);
+            imgCrystal.attr("src", crystalArray[i].picURL);
 
             // Add data-value attribute to hold the value of each crystal
             imgCrystal.attr("data-value", random);
